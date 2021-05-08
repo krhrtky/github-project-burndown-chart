263a14c243ceebc7755e5eae89c68d17a85864b1
t-kurihara <takuya.kurihara00@gmail.com>
2021-05-08 17:08:41 +0900

    Turn off basic auth.

---
+++ api/src/main/kotlin/api/config/SecurityConfig.kt
@@ -0,0 +1,19 @@
+package api.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+class SecurityConfig {
+    @Bean
+    fun securityWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain = http
+        .authorizeExchange()
+        .anyExchange()
+        .permitAll()
+        .and()
+        .build()
+}
