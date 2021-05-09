66eb549b384e58248ac460b99996a6127b31d4d4
t-kurihara <takuya.kurihara00@gmail.com>
2021-05-09 23:40:28 +0900

    Add GitHub project number specification.

--- api/src/main/kotlin/api/domains/models/project/ProjectNumber.kt
+++ api/src/main/kotlin/api/domains/models/project/ProjectNumber.kt
@@ -1,3 +1,9 @@
 package api.domains.models.project
 
-data class ProjectNumber(val value: Int)
+data class ProjectNumber(val value: Int) {
+    init {
+        if(value < 0) {
+            throw IllegalArgumentException("${this.javaClass.simpleName} must be positive number.")
+        }
+    }
+}
