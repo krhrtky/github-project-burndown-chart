1d7c6a4bc4164262d16fe945c8ddad5b3298b814
t-kurihara <takuya.kurihara00@gmail.com>
2021-06-01 23:48:41 +0900

    Enable analytics.

--- client/src/libs/firebase/firebase.ts
+++ client/src/libs/firebase/firebase.ts
@@ -1,11 +1,13 @@
 import { getApps, initializeApp } from "@firebase/app";
 import { initializePerformance } from "@firebase/performance";
+import { getAnalytics } from "@firebase/analytics";
 import { firebaseConfig } from "@/libs/env";
 
 const initializeFirebase = () => {
   if (getApps().length == 0) {
     const app = initializeApp(firebaseConfig);
     initializePerformance(app);
+    getAnalytics();
   }
 }
 
