da1a0e9f2289a52b3ae5ce2c487e46d07cb46c22
t-kurihara <takuya.kurihara00@gmail.com>
2021-06-01 23:49:12 +0900

    Add toast on success register task.

--- client/src/pages/projects/Task/CreateTaskModal.tsx
+++ client/src/pages/projects/Task/CreateTaskModal.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import {Input, Modal} from "@geist-ui/react";
+import React, {useState} from "react";
+import {Input, Modal, useToasts} from "@geist-ui/react";
 import {useParams} from "react-router-dom";
 import {useFormik} from "formik";
 import {TaskControllerService} from "@/generated/openapi";
@@ -12,22 +12,35 @@ type Props = {
 
 export const CreateTaskModal: React.FC<Props> = ({ open, onClose, projectCardId }) => {
   const { projectId } = useParams<{ projectId: string; }>();
+  const [loading, setLoading] = useState(false);
+  const [, setToast] = useToasts();
+  const toastSuccess = () => setToast({
+    text: "Task created !",
+    type: "success",
+  })
   const { values, handleSubmit, handleChange } = useFormik<{estimateStoryPoint?: string }>({
     initialValues: {
       estimateStoryPoint: undefined,
     },
     onSubmit: async values => {
-      await TaskControllerService
-        .create1(
-          projectId,
-          {
-            projectCardId: Number(projectCardId),
-            estimateStoryPoint: Number(values.estimateStoryPoint)
-          });
+      setLoading(true);
+      try {
+        await TaskControllerService
+          .create1(
+            projectId,
+            {
+              projectCardId: Number(projectCardId),
+              estimateStoryPoint: Number(values.estimateStoryPoint)
+            });
+        toastSuccess();
+      } finally {
+        setLoading(false);
+        onClose();
+      }
     }
   });
   return (
-    <Modal open={open} onClose={onClose}>
+    <Modal open={open} onClose={onClose} disableBackdropClick={loading}>
       <Modal.Title>Register task</Modal.Title>
       <Modal.Content>
         <Input
@@ -38,8 +51,8 @@ export const CreateTaskModal: React.FC<Props> = ({ open, onClose, projectCardId 
           onChange={handleChange}
         />
       </Modal.Content>
-      <Modal.Action passive onClick={onClose}>Cancel</Modal.Action>
-      <Modal.Action onClick={() => handleSubmit()}>Register</Modal.Action>
+      <Modal.Action passive onClick={onClose} disabled={loading}>Cancel</Modal.Action>
+      <Modal.Action onClick={() => handleSubmit()} loading={loading}>Register</Modal.Action>
     </Modal>
   );
 }
