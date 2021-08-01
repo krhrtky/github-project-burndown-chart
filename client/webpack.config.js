be8560b90364d9be6666f4f8304f2026941282db
t-kurihara <takuya.kurihara00@gmail.com>
2021-08-01 23:05:31 +0900

    Update Webpack chunk config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,58 +1,58 @@
 /**
  * @type import('webpack').Configuration
  */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {EnvironmentPlugin} = require("webpack");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { resolve } = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { EnvironmentPlugin } = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { resolve } = require("path");
 
 module.exports = (env, argv) => {
-  const mode = argv.mode;
-  const isProduction = mode === 'production';
+  const { mode } = argv;
+  const isProduction = mode === "production";
 
   return {
-    mode: mode,
-    entry: './src/App.tsx',
+    mode,
+    entry: "./src/App.tsx",
     output: {
       path: `${__dirname}/dist`,
-      filename: '[name].[hash].js',
+      filename: "[name].[fullhash].js",
       publicPath: "/",
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          use: 'ts-loader',
+          use: "ts-loader",
         },
       ],
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json'],
+      extensions: [".ts", ".tsx", ".js", ".json"],
       alias: {
-        '@': resolve(__dirname, 'src'),
+        "@": resolve(__dirname, "src"),
       },
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: "./index.html",
       }),
       new CleanWebpackPlugin({
-        cleanAfterEveryBuildPatterns: ['dist'],
+        cleanAfterEveryBuildPatterns: ["dist"],
       }),
       new EnvironmentPlugin([
-          "FIREBASE_API_KEY",
-          "FIREBASE_AUTH_DOMAIN",
-          "FIREBASE_PROJECT_ID",
-          "FIREBASE_STORAGE_BUCKET",
-          "FIREBASE_MESSAGING_SENDER_ID",
-          "FIREBASE_APP_ID",
-          "FIREBASE_MEASUREMENT_ID",
+        "FIREBASE_API_KEY",
+        "FIREBASE_AUTH_DOMAIN",
+        "FIREBASE_PROJECT_ID",
+        "FIREBASE_STORAGE_BUCKET",
+        "FIREBASE_MESSAGING_SENDER_ID",
+        "FIREBASE_APP_ID",
+        "FIREBASE_MEASUREMENT_ID",
       ]),
     ],
     devServer: {
       open: true,
       historyApiFallback: true,
     },
-    devtool: isProduction ? false : 'source-map',
+    devtool: isProduction ? false : "source-map",
   };
 };
